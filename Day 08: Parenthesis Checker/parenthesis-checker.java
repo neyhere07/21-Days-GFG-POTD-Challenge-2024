18e19f0a52655b4d624db6d029d8e3bb0814cda4
Neyna Nayak <140084295+neyhere07@users.noreply.github.com>
2024-09-18 21:34:35 +0530

    day8-parenthesis-checker.java

---
+++ Day 08: Parenthesis Checker/parenthesis-checker.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        // add your code here
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : x.toCharArray()){
+            if(c == '{' || c == '[' || c == '('){
+                stack.push(c);
+            } else if (stack.isEmpty()){
+                return false;
+            } else if ((c == '}' && stack.peek() == '{') ||
+                        (c == ']' && stack.peek() == '[') || 
+                        (c == ')' && stack.peek() == '(')) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
